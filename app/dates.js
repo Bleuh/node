c49de0ee77aa9ff14bc27714969544f90cc2cb4a
Bastien <bastiendouib@gmail.com>
2017-02-24 11:15:57 +0100

    Init nodejs with test lib

---
+++ app/dates.js
@@ -0,0 +1,14 @@
+exports.dateLisible = function(date){
+	var monthNames = ['janvier', 'février', 'mars', 'avril', 'mai', 'juin', 'juillet',
+                'août', 'septembre', 'octobre', 'novembre', 'décembre'];
+	var year = date.getFullYear();
+	var month = date.getMonth();
+	var day = date.getDate();
+	var dateReturn = day + " " + monthNames[month] + " " + year;
+  	return dateReturn ;
+};
+
+exports.date1000 = function(date){
+	date.setDate(date.getDate() + 1000);
+  	return this.dateLisible(date) ;
+}; 
---
+++ app/index.js
@@ -0,0 +1,26 @@
+//app/index.js 
+
+var http = require('http') ;
+var url = require('url') ;
+var dates = require('./dates') ;
+
+var monServeur=function(requete, reponse){
+  	var page = url.parse(requete.url).pathname ;
+	var sortie ;
+
+	var slug = page.split('/');
+	var date = new Date(slug);
+	if(date != "Invalid Date"){
+		reponse.writeHead(200,{"Content-Type": "text/plain; charset=UTF-8"}); 
+		sortie = "La date est : " + dates.date1000(date);
+	}
+	else{
+		reponse.writeHead(404);
+		sortie="Erreur 404";
+	}
+ 	reponse.end(sortie) ;
+}
+
+var serveur = http.createServer(monServeur) ;
+
+serveur.listen(8080) ; 
